251afad8eeffa2459b8642c2b777b1096431624e
tonegi <90652377+tonegi@users.noreply.github.com>
2025-06-03 23:30:13 -0300

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,17 +1,25 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("dot");
 
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -19,10 +27,4 @@ function showSlides() {
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000); // Muda a cada 5 segundos
-}
-
-function currentSlide(n) {
-    slideIndex = n - 1;
-    showSlides();
 }
